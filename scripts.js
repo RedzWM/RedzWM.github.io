bf5ca684e44b5be5a73c357e8814904d8c00ada5
RedzWM <47967775+RedzWM@users.noreply.github.com>
2022-12-15 12:36:07 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
-function myFunction() {
-  var num = document.getElementById("num").value;
+/* Place your JavaScript in this file */
+function getForm() {
+var num = document.getElementById("num").value;
   var digit = document.getElementById("digit").value;
   submitOK = "true";
+
+var max = "9"
+for (let i=1; i < digit; i++){
+    max = max + "9"
+}
+    return {num,max}
   }
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function showNum() {
+    let data = getForm()
+    let num = data.num
+    for(let i=1; i<=num; i++){
+        let number = "num"+[i]
+
+    document.getElementById(number).innerHTML = (getRandomInt(0,parseInt(data.max))).toString().padStart(data.max.length,'0')
+    }
 }
